6c58f4e58fc0ea6c1c259fbb2281f507ec4cda3a
Renzo <nviscio@gmail.com>
2022-11-12 15:25:49 -0300

    sadasd

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+const hamburguer = document.querySelector('.hamburguer')
+const menu = document.querySelector('.menu-navegacion') //Constantes creadas para encerrar a los elementos del html
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+}); /*Adhiere o quita la clase "spread" a la hamburguesa del nav cada vez que se clickea*/
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && e.target != menu && e.target != hamburguer   ){
+        menu.classList.toggle("spread")
+    }
+}); /*Si el menú tiene la clase spread y el elemento al que le estamos dando click es diferente al menú y a la hamburguesa,
+el nav se esconde si clickeamos cualquier parte de la página que no sea el nav*/
